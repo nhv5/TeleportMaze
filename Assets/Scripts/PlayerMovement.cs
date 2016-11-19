cdedbbb09284dbb7dcd06734beda27244ef6fd43
Nicholas Vuono <nhv5@scarletmail.rutgers.edu>
2016-11-19 00:38:56 -0500

    Portal is working!
    Added new scene to test portal block. It correctly send the player to
    the opposite portal on the island platform.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour {
 
     public float speed;
-    private float topSpeed = 4f;
+    private float topSpeed = 3f;
     public static int level = 0;
+    private Vector3 spawn;
+    public GameObject tele;
 
     // Use this for initialization
     void Start () {
 
-	}
+        spawn = transform.position;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -24,18 +26,65 @@ public class PlayerMovement : MonoBehaviour {
 
         if (GetComponent<Rigidbody>().velocity.magnitude < topSpeed) GetComponent<Rigidbody>().AddForce(movement * speed);
 
-    }
-
-    void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.CompareTag("Up")) transform.position = new Vector3(0.0f, 0.0f, transform.position.z + 3.0f); ;
+        if (transform.position.y < -3) Teleport();
 
     }
 
     void OnTriggerEnter(Collider other)
     {
-        level += level;
-        if (other.gameObject.CompareTag("End")) SceneManager.LoadScene(level);
+        if (other.gameObject.CompareTag("End")) Main.NextLevel();
+
+        if (other.gameObject.CompareTag("Up")) Teleport("up");
+
+        if (other.gameObject.CompareTag("Down")) Teleport("down");
+
+        if (other.gameObject.CompareTag("Left")) Teleport("left");
+
+        if (other.gameObject.CompareTag("Right")) Teleport("right");
+
+        if (other.gameObject.CompareTag("Portal")) Teleport("portal");
+
     }
+
+    void Teleport(string direction)
+    {
+        if (direction.Equals("up"))
+        {
+            Instantiate(tele, transform.position, Quaternion.identity);
+            transform.position = new Vector3(transform.position.x, 0.1f, transform.position.z + 2.0f);
+        }
+
+        if (direction.Equals("down"))
+        {
+            Instantiate(tele, transform.position, Quaternion.identity);
+            transform.position = new Vector3(transform.position.x, 0.1f, transform.position.z - 2.0f);
+        }
+
+        if (direction.Equals("left"))
+        {
+            Instantiate(tele, transform.position, Quaternion.identity);
+            transform.position = new Vector3(transform.position.x - 2.0f, 0.1f, transform.position.z);
+        }
+
+        if (direction.Equals("right"))
+        {
+            Instantiate(tele, transform.position, Quaternion.identity);
+            transform.position = new Vector3(transform.position.x + 2.0f, 0.1f, transform.position.z);
+        }
+
+        if (direction.Equals("portal"))
+        {
+            Instantiate(tele, transform.position, Quaternion.identity);
+            transform.position = new Vector3(GameObject.FindGameObjectWithTag("Portal2").transform.position.x, 0.1f, GameObject.FindGameObjectWithTag("Portal2").transform.position.z);
+        }
+    }
+
+    void Teleport()
+    {
+        Instantiate(tele, transform.position, Quaternion.identity);
+        transform.position = spawn;
+    }
+
+
 
 }
