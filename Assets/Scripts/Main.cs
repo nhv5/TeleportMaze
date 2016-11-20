33f924329167a946927dd91d64e9f742845f98df
Nicholas Vuono <nhv5@scarletmail.rutgers.edu>
2016-11-19 19:51:14 -0500

    Added Title Page!

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,14 +4,24 @@ using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour {
 
-    public static int level = 0;
+    public static int level = 1;
+    private float timer = 4.0f;
+
+    void FixedUpdate()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+
+            timer -= Time.deltaTime;
+
+            if (timer <= 0) SceneManager.LoadScene(1);
+        }
+    }
 
     public static void NextLevel()
     {
-
         level += 1;
         SceneManager.LoadScene(level);
     }
 
-	
 }
